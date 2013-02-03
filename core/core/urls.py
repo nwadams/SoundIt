21ea7147c539ed782ad694029a8a110ada021e1c
Anuj <anuj.nm@gmail.com>
2013-02-02 22:18:28 -0800

    Re-adding admin config.

--- core/core/urls.py
+++ core/core/urls.py
@@ -6,5 +6,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     
     url(r'^backend/', include('backend.urls')),
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
