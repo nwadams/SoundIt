8cf745b7856afef18c38fe7c8d95fd5c1376c253
Anuj <anuj.nm@gmail.com>
2013-02-02 17:26:45 -0800

    Commenting out admin related config. This was causing problems with the API.

--- core/backend/admin.py
+++ core/backend/admin.py
@@ -4,26 +4,26 @@ Created on Feb 2, 2013
 @author: anuj
 '''
 
-from django.contrib import admin
-from backend.models import Customer
-from backend.models import Consumer
-from backend.models import Location
-from backend.models import Artist
-from backend.models import Album
-from backend.models import MusicCategory
-from backend.models import MusicTrack
-from backend.models import Playlist
-from backend.models import PlaylistItem
-from backend.models import Vote
+# from django.contrib import admin
+# from backend.models import Customer
+# from backend.models import Consumer
+# from backend.models import Location
+# from backend.models import Artist
+# from backend.models import Album
+# from backend.models import MusicCategory
+# from backend.models import MusicTrack
+# from backend.models import Playlist
+# from backend.models import PlaylistItem
+# from backend.models import Vote
  
 
-admin.site.register(Customer)
-admin.site.register(Consumer)
-admin.site.register(Location)
-admin.site.register(Artist)
-admin.site.register(Album)
-admin.site.register(MusicCategory)
-admin.site.register(MusicTrack)
-admin.site.register(Playlist)
-admin.site.register(PlaylistItem)
-admin.site.register(Vote)
+# admin.site.register(Customer)
+# admin.site.register(Consumer)
+# admin.site.register(Location)
+# admin.site.register(Artist)
+# admin.site.register(Album)
+# admin.site.register(MusicCategory)
+# admin.site.register(MusicTrack)
+# admin.site.register(Playlist)
+# admin.site.register(PlaylistItem)
+# admin.site.register(Vote)
