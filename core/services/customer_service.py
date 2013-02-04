e441b5b0b3695245f6708f334f8d25557aab2960
Anuj <anuj.nm@gmail.com>
2013-02-04 02:31:18 -0800

    Added more logging to customer_service.

--- core/services/customer_service.py
+++ core/services/customer_service.py
@@ -7,6 +7,9 @@ Created on Jan 21, 2013
 from backend.models import Customer 
 import hashlib
 import random
+import logging
+
+logger = logging.getLogger('core.backend')
 
 class CustomerService:
     
@@ -14,15 +17,18 @@ class CustomerService:
         try:
             customer = Customer.objects.get(device_id=device_id)
         except Customer.DoesNotExist:
+            logger.warning("Could not find customer with device " + str(device_id))
             return False
-        # this might be redundant. Verify.
+        # this is probably redundant. Verify.
         if customer is None:
+            logger.warning("Could not find customer with device " + str(device_id))
             return False
         hashed_password = hashlib.md5(customer.salt + password).hexdigest()
         return hashed_password == customer.password
 
     def register(self, device_id, password):
         # TODO: Improve password salting
+        logger.debug("Registering customer with device " + str(device_id))
         salt = str(random.randint(10,99))
         hashed_password = hashlib.md5(salt + password).hexdigest()
         new_customer = Customer(device_id=device_id, password=hashed_password, salt=salt)
@@ -30,6 +36,7 @@ class CustomerService:
         return new_customer
     
     def checkIfCurrentCustomer(self, device_id):
+        logger.debug("Checking if user with device id " + str(device_id) + " already exists.")
         try: 
             Customer.objects.get(device_id = device_id)
         except (KeyError, Customer.DoesNotExist):
